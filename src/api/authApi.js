e6ba68fd555d94897f16f701f1f0f8de72a2a149
Serhii457 <manushenko7@gmail.com>
2025-08-10 13:20:33 +0300

    final fix

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,4 @@
-// const BASE_URL = 'http://localhost:8080';
-// const BASE_URL = 'https://health-life-app-540005833b57.herokuapp.com/';
 const BASE_URL = window.location.origin;
-
 
 export async function login(username, password) {
   const response = await fetch(`${BASE_URL}/login`, {
