528e009319f62a02b7c7e30d220a70693c3ac78e
Serhii457 <manushenko7@gmail.com>
2025-08-12 02:24:59 +0300

    change location.css, footer.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import "./styles/Footer.css";
 
 const Footer = () => {
 
@@ -13,7 +14,7 @@ const Footer = () => {
     <div className="btFooterWrap bg-dark text-light pt-5">
       <section className="container pb-5">
         <div className="row">
-          <div className="col-md-4 mb-4 text-md-centre">
+          <div className="col-md-4 mb-4 text-md-center">
             <img
               src="./logo_health_life.png"
               alt="Логотип клініки"
@@ -35,13 +36,13 @@ const Footer = () => {
             </p>
           </div>
 
-          <div className="col-md-4 mb-4 text-md-centre">
+          <div className="col-md-4 mb-4 text-md-center">
             <p className="text-justify ps-5">
               Увага! Вся інформація на цьому сайті ( фото, статті, відео) носить виключно інформаційний характер і ні при яких умовах не є публічною офертою згідно статті 641 Цивільного кодексу України. Всі права на базу данних, що відносяться до даного веб-сайту, а також вся інформація, розміщена на ньому, включаючи графічні зображення, статті, відео, фотографії належать власнику веб-сайту. Будь – яке використання інформації статей чи їх частин, фотографій, можливе тільки з вказанням прямого посилання на матеріал. Всі авторські права захищені та охороняються законом.
             </p>
           </div>
 
-          <div className="col-md-4 mb-4 text-md-centre">
+          <div className="col-md-4 mb-4 text-md-center">
             <h5 className="mb-3 ms-5 ps-5">Наші послуги</h5>
             <div className="d-flex flex-column align-items-md-centre gap-2">
               <ul className="list-unstyled ms-5 ps-5">
